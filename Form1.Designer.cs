b9dbcbea781c91ce669ccab6841995442cd9cc31
kana <1612998058@qq.com>
2019-02-23 20:33:30 +0800

    初始代码
    修改注册表达到隐藏显示分区的目的

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace DiskHide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(338, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "隐藏所选盘符";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(338, 38);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "显示所有盘符";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(30, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "A";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(68, 22);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(30, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "B";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(110, 22);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(30, 16);
+            this.checkBox3.TabIndex = 1;
+            this.checkBox3.Text = "C";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(152, 22);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(30, 16);
+            this.checkBox4.TabIndex = 1;
+            this.checkBox4.Text = "D";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(194, 22);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(30, 16);
+            this.checkBox5.TabIndex = 1;
+            this.checkBox5.Text = "E";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(236, 22);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(30, 16);
+            this.checkBox6.TabIndex = 1;
+            this.checkBox6.Text = "F";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(278, 22);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(30, 16);
+            this.checkBox7.TabIndex = 1;
+            this.checkBox7.Text = "G";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(320, 22);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(30, 16);
+            this.checkBox8.TabIndex = 1;
+            this.checkBox8.Text = "H";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 155);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "分区隐藏助手 byQQ1612998058";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiskHide
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DiskHide(byte[] Values)
+        {
+            try
+            {
+                RegistryKey rgK = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+                rgK.SetValue("NoDrives", Values, RegistryValueKind.Binary);//给默认值赋值
+                MessageBox.Show("OK");
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 以杀死进程的方式杀死explorer
+        /// </summary>
+        /// <param name="name">要杀死的进程友好名（无 .exe）</param>
+        static public void killProcess(string name)
+        {
+            try
+            {
+                Process[] myProcesses = Process.GetProcesses();
+                foreach (Process myProcess in myProcesses)
+                {
+                    if (name == myProcess.ProcessName)
+                        myProcess.Kill();
+                }
+            }
+            catch (Exception ee)
+            {
+                //抛出异常
+                MessageBox.Show(ee.Message);
+            }
+        }
+        #region 第一种运行CMD的方式
+        /// <summary>
+        /// 调用CMD命令来杀死或重启进程
+        /// </summary>
+        /// <param name="a">杀死或重启进程</param>
+        /// <returns>cmd命令返回</returns>
+        public static void cmdkill(bool a)
+        {
+            string str;
+            //string str = Console.ReadLine();
+            if (a)
+            {
+                str = @"taskkill /f /im explorer.exe";
+            }
+            else
+            {
+                str = @"C:\Windows\explorer.exe";
+                //str = @"explorer.exe";
+            }
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd";
+            p.StartInfo.UseShellExecute = false;                 //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;            //接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;           //由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;            //重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;                   //不显示程序窗口
+            p.Start();                                           //启动程序
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(str + "&exit");
+            //p.StandardInput.WriteLine(str);
+            p.StandardInput.AutoFlush = true;
+            //p.StandardInput.WriteLine("exit");
+            //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
+            //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
+
+            //获取cmd窗口的输出信息
+            //string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();//等待程序执行完退出进程
+            p.Close();
+            //return output;
+        }
+        #endregion
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            /*
+             * 01表示A盘、02表示B盘、04表示C盘、08表示D盘、10表示E盘、20表示F盘、40表示G盘、80表示H盘。如果要隐藏F盘时，将数值数据设置为“20000000
+另一个方法隐藏硬盘分区
+运行输入gpedit.msc回车打开组策略，在左侧选用户配置/管理模板/Windows组件/Windows资源管理器/在右侧选“隐藏“我的电脑”中的这些指定的驱动器”，双击它在打开的对话框中选择“已启用”，然后将你要隐藏的盘符添进去，然后按应用确定。通过设置随然隐藏了盘符，但是还可以从我的电脑地址栏访问隐藏的盘符。可通过设置禁止这样做，在右侧还有一项“防止从我的电脑访问驱动器”双击它在打开的对话框中选择“已启用”然后选择你要禁止访问的盘符添进去后，按应用确定，重启电脑即可。
+*/
+            byte data = 00;
+            if (checkBox1.Checked)
+                data += 0x01;
+            if (checkBox2.Checked)
+                data += 0x02;
+            if (checkBox3.Checked)
+                data += 0x04;
+            if (checkBox4.Checked)
+                data += 0x08;
+            if (checkBox5.Checked)
+                data += 0x10;
+            if (checkBox6.Checked)
+                data += 0x20;
+            if (checkBox7.Checked)
+                data += 0x40;
+            if (checkBox8.Checked)
+                data += 0x80;
+
+            byte[] bvalues = new byte[] { data, 00, 00, 00 }; //隐藏F盘
+            DiskHide(bvalues);
+            //重启 explorer
+            cmdkill(true);
+            cmdkill(false);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            byte[] bvalues = new byte[] { 00, 00, 00, 00 };
+            DiskHide(bvalues);
+            //重启 explorer
+            cmdkill(true);
+            cmdkill(false);
+        }
+    }
+}
